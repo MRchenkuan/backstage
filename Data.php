471ce72348a9d460572c7b6e3e185eb9435bd90c
MRchenkuan <393667111@qq.com>
2015-05-29 13:17:10 +0800

    创建
    首次添加、文件上传功能

---
+++ Data.php
@@ -0,0 +1,42 @@
+<?
+	$APIID = $_GET['id']?$_GET['id']:'defaultMethod';
+
+	$config=array();
+	$config['defaultMethod']=defaultMethod;
+	$config['getNews']=getNews;
+	$config['uploadImg']=uploadImg;
+	$config[$APIID]();
+
+
+
+	function getNews()
+	{
+		echo 'this is '.$_GET['param']?$_GET['param']:'api unformated!'.'s news';
+	}
+
+	function defaultMethod(){
+		echo 'api unformated!';
+	}
+
+	function uploadImg(){
+		$uploaddir = './'.date('Ymd').'/';
+		if(!file_exists($uploaddir)){
+			if(mkdir($uploaddir)){
+				echo $uploaddir.' created!<br>';
+			}else{
+				echo 'faile to create '.$uploaddir.'maybe the path you have no permit!<br>';
+			};
+		}
+		$uploadfile = $uploaddir.$_FILES['userfile']['name'];
+		echo $uploadfile;
+		print "<pre>";
+		if (move_uploaded_file($_FILES['userfile']['tmp_name'],$uploaddir.$_FILES['userfile']['name'])) {
+		    print "File is valid, and was successfully uploaded.  Here's some more debugging info:\n";
+		    print_r($_FILES);
+		} else {
+		    print "Possible file upload attack!  Here's some debugging info:\n";
+		    print_r($_FILES);
+		}
+		print "</pre>";
+	}
+?>
