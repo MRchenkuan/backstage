652bb539a0db49d7322f1a3a2d09e418333b1947
chenkuan <chenkuan@gigold.com>
2016-03-11 18:18:55 +0800

    New changelist

---
+++ news-idea.php
@@ -0,0 +1,36 @@
+<!--here is head-->
+<?php
+error_reporting(0);
+session_start();
+$pageID='news';
+include "./widgets/head.php";
+?>
+
+<!--content-->
+<?php
+/*连接数据库*/
+require_once('./tools/Kodbc.class.php');
+$newsid = $_GET['id'];
+$kodbc = new Kodbc('./Database/IDEADATA.xml');
+
+$news = $kodbc->getById($newsid);
+?>
+
+<div class="panel panel-default" style="width: 960px;margin: 60px auto 0 auto">
+    <!-- Default panel contents -->
+    <div class="panel-heading"><span class="glyphicon glyphicon-calendar"></span> 新闻预览</div>
+    <div class="panel-body">
+    <?php
+
+        $newsfile = fopen($news['text'],'r') or die('can not find news,because no news file found');
+        echo htmlspecialchars_decode(fread($newsfile,filesize($news['text'])));
+        fclose($newsfile);
+    ?>
+    </div>
+</div>
+
+
+<!--here this foot-->
+<?php
+include "./widgets/foot.php";
+?>
---
+++ newstxt-idea.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head lang="zh-CN">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="./bootstrap/bootstrap-theme.min.css">
+
+    <script src="./js/jquery.min.js"></script>
+    <script src="./bootstrap/bootstrap.min.js"></script>
+    <style>
+        .input-group{margin: 10px auto;}
+    </style>
+    <title></title>
+</head>
+<body style="overflow-x: hidden;padding: 20px 5%;">
+<!--content-->
+<?php
+/*连接数据库*/
+require_once('./tools/Kodbc.class.php');
+$newsid = $_GET['id'];
+$kodbc = new Kodbc('./Database/IDEADATA.xml');
+
+$news = $kodbc->getById($newsid);
+
+$newsfile = fopen($news['text'],'r') or die('can not find news,because no news file found');
+echo htmlspecialchars_decode(fread($newsfile,filesize($news['text'])));
+fclose($newsfile);
+?>
+</body>
+</html>
